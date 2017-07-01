ebd5364b6638e7ae1c6540c2e07acc809373280a
Frank Meza <frankmezapdx@gmail.com>
2017-07-01 15:58:33 -0700

    auth token returned on successful login

--- controllers/auth_ctrl.rb
+++ controllers/auth_ctrl.rb
@@ -4,9 +4,13 @@ AuthCtrl = Cuba.new do
   on post, 'auth' do
     input = req.body.read
     body = JSON.parse(input)
+
     if login User, body['login'], body['password']
-    # user = User.authenticate body['login'], body['password']
-    res.write user.to_json
+      token = JWT.encode body['login'], body['password'], 'HS256'
+      auth_token = { token: token }
+    end
+
+    res.write auth_token.to_json
   end
 end
 
