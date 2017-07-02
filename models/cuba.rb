36bf5f781bf0eb2171b9a39c2d5ec37729ddfcb1
Frank Meza <frankmezapdx@gmail.com>
2017-07-02 11:21:30 -0700

    hoist module

--- models/cuba.rb
+++ models/cuba.rb
@@ -1,10 +1,10 @@
-class Cuba
-  include Helpers
-end
-
+# helpers
 module Helpers
   def set_response_as_json
     res.headers["Content-Type"] = "application/json"
   end
 end
 
+class Cuba
+  include Helpers
+end
