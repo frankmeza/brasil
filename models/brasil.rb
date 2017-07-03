d4170bf44037be257c873892715f6336b5216850
Frank Meza <frankmezapdx@gmail.com>
2017-07-03 16:54:07 -0700

    adjust indent, add 401 method

--- models/brasil.rb
+++ models/brasil.rb
@@ -1,31 +1,38 @@
+module ResponseHelpers
+  def set_response_as_json
+    res.headers['Content-Type'] = 'application/json'
+  end
+
+  def set_response_status(code)
+    res.status = code
+  end
+end
+
+module AuthHelpers
+  def has_jwt?
+    env['HTTP_JWT_TOKEN'].present?
+  end
+
+  def encode_data(data)
+    JWT.encode data, ENV['JWT_SECRET'], 'HS256'
+  end
+
+  def decode_jwt_token(token)
+    JWT.decode token, ENV['JWT_SECRET'], true, { algorithm: 'HS256' }
+  end
+
+  def respond_with_401
+    ['401', {"Content-Type" => "application/json"}, [Message.error('jwt_missing').to_json]]
+  end
+
+  def is_valid_token?(token)
+    data = decode_jwt_token(token)[0]
+    user = User.fetch data['email']
+    user.username == data['username']
+  end
+end
+
 class Brasil < Cuba
-  # module ResponseHelpers
-    def set_response_as_json
-      res.headers['Content-Type'] = 'application/json'
-    end
-
-    def set_response_status(code)
-      res.status = code
-    end
-  # end
-
-  # module AuthHelpers
-    def has_jwt?
-      ENV['HTTP_JWT_TOKEN'].present?
-    end
-
-    def encode_data(data)
-      JWT.encode data, ENV['JWT_SECRET'], 'HS256'
-    end
-
-    def decode_jwt_token(token)
-      JWT.decode token, ENV['JWT_SECRET'], true, { algorithm: 'HS256' }
-    end
-
-    def is_valid_token?(token)
-      data = decode_jwt_token(token)
-      user = User.fetch data['email']
-      user.username == data['username']
-    end
-  # end
+  include AuthHelpers
+  include ResponseHelpers
 end
