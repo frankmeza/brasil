4d67f0369582edde3929dbb63b2286a4cab182b8
Frank Meza <frankmezapdx@gmail.com>
2017-07-04 12:25:49 -0700

    adjust ENV and global variables

--- models/brasil.rb
+++ models/brasil.rb
@@ -10,26 +10,29 @@ end
 
 module AuthJwtHelpers
   def has_jwt?
-    env['HTTP_JWT_TOKEN'].present?
+    env['JWT_TOKEN'].present?
   end
 
   def encode_data(data)
-    JWT.encode data, ENV['JWT_SECRET'], 'HS256'
+    JWT.encode data, $JWT_SECRET, 'HS256'
   end
 
   def decode_jwt_token(token)
-    JWT.decode token, ENV['JWT_SECRET'], true, { algorithm: 'HS256' }
+    JWT.decode token, $JWT_SECRET, true, { algorithm: 'HS256' }
   end
 
   def respond_with(status, message_key)
-    ['401', {"Content-Type" => "application/json"}, [Message.get_msg(message_key).to_json]]
+    # does it have to be {"Content-Type" => "application/json"} ?
+    [status.to_s, {}, [Message.get_msg(message_key)]]
   end
 
   def is_valid_token?(token)
-    data = decode_jwt_token(token)[0]
-    user = User.fetch data['email']
-    user.username == data['username']
+    data = decode_jwt_token(token)
+    user = User.fetch data[0]['email']
+    user.username == data[0]['username']
   end
+
+  # def fetch_user_from_token
 end
 
 class Brasil < Cuba
