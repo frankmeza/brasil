736ab0eb3ea27e5dc4e33eefe71aa5eb7de5a6b0
Frank Meza <frankmezapdx@gmail.com>
2017-07-05 14:33:39 -0700

    add test admin and user, each with JWT_TOKEN

--- seeds.rb
+++ seeds.rb
@@ -14,11 +14,43 @@ Mongoid::Config.purge!
 #  seed data  #
 # # # # # # # #
 
-# User
-100.times do
+
+admin = User.create!(
+  username: 'ADMIN',
+  email: 'admin@email.com',
+  password: 'password',
+  is_admin: true
+)
+
+# {:username=>"ADMIN", :email=>"admin@email.com"}
+# JWT_TOKEN: eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJ1c2VybmFtZSI6IkFETUlOIiwiZW1haWwiOiJhZG1pbkBlbWFpbC5jb20ifQ.4XSL_eFP5X2odvYn4zKJa7oMxWSOgLVgs4bQUTZKkOU
+
+alpha = User.create!(
+  username: 'alpha',
+  email: 'alpha@email.com',
+  password: 'password'
+)
+
+{:username=>"alpha", :email=>"alpha@email.com"}
+# JWT_TOKEN: eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJ1c2VybmFtZSI6ImFscGhhIiwiZW1haWwiOiJhbHBoYUBlbWFpbC5jb20ifQ.rqbMh-9KihAuLJmKAyHWptmy2DJMnLA-nhH9u4DRW9A
+
+# Users
+98.times do
   User.create!(
     username: Faker::Name.first_name,
     email: Faker::Internet.email,
-    password: 'password'
+    password: 'password',
+    is_admin: false
   )
 end
+
+puts 'Seeding is complete.'
+puts 'Admin data: #=>'
+puts admin.get_data
+puts "JWT_TOKEN: #{AuthCtrl.encode_data(admin.get_data)}"
+
+puts '##########'
+
+puts 'Alpha User data: #=>'
+puts alpha.get_data
+puts "JWT_TOKEN: #{AuthCtrl.encode_data(alpha.get_data)}"
