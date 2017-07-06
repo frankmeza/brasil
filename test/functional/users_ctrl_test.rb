4ce0c864d924d6a0c67cef92cf5c189eee5e0f8f
Frank Meza <frankmezapdx@gmail.com>
2017-07-05 17:22:23 -0700

    refactor tests for admin only success

--- test/functional/users_ctrl_test.rb
+++ test/functional/users_ctrl_test.rb
@@ -4,30 +4,28 @@ class RackTest
 
   def setup
     @user = create(:user)
-    token = AuthCtrl.encode_data(@user.get_data)
-    @jwt_user_header = {"HTTP_JWT_TOKEN" => token}
-
     @admin = create(:user, :admin)
-    token = AuthCtrl.encode_data(@admin.get_data)
-    @jwt_admin_header = {"HTTP_JWT_TOKEN" => token}
+    @users = create_list(:user, 2)
   end
 
   def teardown
     clean_data
   end
 
-  def test_get_users
-    users = create_list(:user, 9)
-    # get '/users', {}, @jwt_user_header
-    get '/users', {}, @jwt_admin_header
+  def test_users_root_admin_success
+    admin_token = AuthCtrl.encode_data(@admin.get_data)
+    get '/users', {}, {"HTTP_JWT_TOKEN" => admin_token}
 
     assert_equal(200, res.status)
-    # the +1 accounts for the user created in setup
-    assert_equal(users.size + 2, res_as_json['users'].size)
+    # the +2 accounts for @user and @admin
+    assert_equal(@users.size + 2, res_as_json['users'].size)
   end
 
-  # def test_post_users
-  #   post '/users/lit', {lit: "lit"}
-  #   assert_equal 200, res.status
-  # end
+  def test_users_root_user_fail
+    user_token = AuthCtrl.encode_data(@user.get_data)
+    get '/users', {}, {"HTTP_JWT_TOKEN" => user_token}
+
+    assert_equal(403, res.status)
+    assert_equal 'Your token is not an admin token.', res_as_json
+  end
 end
