b6896d4f47f00da1fea1d1e1539aad8d166a1899
Frank Meza <frankmezapdx@gmail.com>
2017-07-06 16:34:30 -0700

    add tests for new methods

--- test/functional/users_ctrl_test.rb
+++ test/functional/users_ctrl_test.rb
@@ -6,26 +6,44 @@ class RackTest
     @user = create(:user)
     @admin = create(:user, :admin)
     @users = create_list(:user, 2)
+    admin_token = AuthCtrl.encode_data(@admin.get_data)
+    @jwt_admin = {"HTTP_JWT_TOKEN" => admin_token}
   end
 
   def teardown
     clean_data
   end
 
+  # GET /users
   def test_users_root_admin_success
-    admin_token = AuthCtrl.encode_data(@admin.get_data)
-    get '/users', {}, {"HTTP_JWT_TOKEN" => admin_token}
+    get '/users', {}, @jwt_admin
 
     assert_equal(200, res.status)
     # the +2 accounts for @user and @admin
     assert_equal(@users.size + 2, res_as_json['users'].size)
   end
 
+  # GET /users
   def test_users_root_user_fail
     user_token = AuthCtrl.encode_data(@user.get_data)
     get '/users', {}, {"HTTP_JWT_TOKEN" => user_token}
 
     assert_equal(403, res.status)
-    assert_equal 'Your token is not an admin token.', res_as_json
+    assert_equal('Your token is not an admin token.', res_as_json)
+  end
+
+  def test_user_show_by_username
+    get "/users/#{@user.username}", {}, @jwt_admin
+
+    assert_equal(200, res.status)
+    assert_equal(@user.email, res_as_json['email'])
+    assert_equal(@user.username, res_as_json['username'])
+  end
+
+  def test_update_user
+    new_username = { username: 'updated' }
+    put "/users/#{@user.id}/edit", new_username, @jwt_admin
+    assert_equal(204, res.status)
+    assert_equal(@user.username, new_username[:username])
   end
 end
