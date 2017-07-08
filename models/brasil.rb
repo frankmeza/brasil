4f4065c70278b49cb772e2ab16af0187f3a8c55e
Frank Meza <frankmezapdx@gmail.com>
2017-07-07 22:29:27 -0700

    add module

--- models/brasil.rb
+++ models/brasil.rb
@@ -1,7 +1,9 @@
 require_relative '../lib/auth_jwt_helpers'
 require_relative '../lib/response_helpers'
+require_relative '../lib/request_helpers'
 
 class Brasil < Cuba
   include AuthJwtHelpers
+  include RequestHelpers
   include ResponseHelpers
 end
