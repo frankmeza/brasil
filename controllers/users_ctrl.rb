a160f373dc6b12fca2f983ddbcfda5fbae72ac74
Frank Meza <frankmezapdx@gmail.com>
2017-07-07 22:45:03 -0700

    cleanup and small refactor

--- controllers/users_ctrl.rb
+++ controllers/users_ctrl.rb
@@ -7,41 +7,34 @@ UsersCtrl = Brasil.new do
     set_response_as_json
 
     on get, root do
-      # set_response_as_json
-
       uu = User.all.map do |u|
         u.serialize(:id, :username, :email)
       end
-
       write_res_as_json({ users: uu })
     end
 
     on get, 'id/:id' do |id|
-      set_response_as_json
       u = User.[](id.to_s)
       user = u.serialize(:id, :username, :email)
-      res.write user.to_json
+      write_res_as_json({ user: u })
     end
 
     on put, 'id/:id/edit' do |id|
-      puts parse_req_as_json
       body = parse_req_as_json
       u = User.find(id)
-
       begin
         u.update(body)
         set_response_status(204)
       rescue => exception
         set_response_status(422)
-        res.write exception
+        res.write(exception)
       end
     end
 
-    on get, ':username' do |username|
-      set_response_as_json
+    on get, '/username/:username' do |username|
       u = User.fetch_by_username(username)
       user = u.serialize(:id, :username, :email)
-      res.write user.to_json
+      write_res_as_json({ user: u })
     end
   end
 end
