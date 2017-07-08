ac4ac603793d95597d7fd9db76a1d55eca69a67a
Frank Meza <frankmezapdx@gmail.com>
2017-07-08 16:40:25 -0700

    space

--- test/functional/auth_ctrl_test.rb
+++ test/functional/auth_ctrl_test.rb
@@ -15,7 +15,6 @@ class RackTest
     credentials = { login: @user.username, password: 'password' }
     post '/auth/login', credentials.to_json
     assert_equal(201, res.status)
-
     user = User.fetch(credentials[:login])
     expected_token = AuthCtrl.encode_data(user.data_for_token)
     assert_equal(expected_token, res_as_json['token'])
@@ -25,7 +24,6 @@ class RackTest
     bad_credentials = { login: @user.username, password: 'wrongpassword' }
     post '/auth/login', bad_credentials.to_json
     assert_equal(401, res.status)
-
     errors = Message.get_msg('credentials_invalid')
     assert_equal(errors.to_json, res.body)
   end
@@ -33,10 +31,8 @@ class RackTest
   # GET '/auth/logout'
   def test_logout
     login_this(@user)
-
     get 'auth/logout'
     assert_equal(200, res.status)
-
     logged_out = { logged_out: true }
     assert_equal(logged_out.to_json, res.body)
   end
